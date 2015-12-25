f03ca3a1654afa1583c8a100750683dfa369f533
Wolf <fang.wolf@qq.com>
2015-12-25 13:22:22 +0800

    test for commit

--- src/main/java/demo/test/Test.java
+++ src/main/java/demo/test/Test.java
@@ -15,6 +15,6 @@ public class Test {
 	public static void main(String[] args) {
 		SimpleDateFormat time=new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
 		String TimeString = time.format(new java.util.Date());
-		System.out.println(TimeString);
+		System.out.println(TimeString); 
 	}
 }
